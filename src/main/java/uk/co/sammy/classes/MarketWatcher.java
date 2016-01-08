b95227403d6cf335d33d9e0340fbb8a651530721
Samuel Ifere <sml.ifere@gmail.com>
2016-01-08 15:45:25 +0000

    Modifies classes adding more methods and change MarketWatcher to interface

--- src/main/java/uk/co/sammy/classes/MarketWatcher.java
+++ src/main/java/uk/co/sammy/classes/MarketWatcher.java
@@ -1,12 +1,8 @@
 package uk.co.sammy.classes;
 
-import java.math.BigDecimal;
-
 /**
  * Created by smlif on 07/01/2016.
  */
-public class MarketWatcher {
-    public Stock getQuote(String symbol) {
-        return new Stock("", "", new BigDecimal(200.00));
-    }
+public interface MarketWatcher {
+    Stock getQuote(String symbol);
 }
--- src/main/java/uk/co/sammy/classes/Portfolio.java
+++ src/main/java/uk/co/sammy/classes/Portfolio.java
@@ -1,18 +1,60 @@
 package uk.co.sammy.classes;
 
 import java.math.BigDecimal;
+import java.util.*;
 
 /**
  * Created by smlif on 07/01/2016.
  */
 public class Portfolio {
+    private Map<String, List<Stock>> stockMap = new HashMap<>();
+
     public BigDecimal getAvgPrice(Stock stock) {
-        return new BigDecimal(200.00);
+        List<Stock> stocks = stockMap.get(stock.getSymbol());
+        BigDecimal avgPrice = BigDecimal.ZERO;
+
+        if(stocks != null){
+            for(Stock aStock : stocks){
+                avgPrice = avgPrice.add(aStock.getPrice());
+            }
+            avgPrice = avgPrice.divide(new BigDecimal(stocks.size()));
+        }
+        return avgPrice;
     }
 
     public void buy(Stock stock) {
+        List<Stock> stocks = stockMap.get(stock.getSymbol());
+        if(stocks != null){
+            stocks.add(stock);
+        }else {
+            stocks = new ArrayList<>();
+            stocks.add(stock);
+            stockMap.put(stock.getSymbol(), stocks);
+        }
     }
 
-    public void sell(Stock stock, int units) {
+    public void sell(Stock stock, int quauntity) {
+        List<Stock> stocks = stockMap.get(stock.getSymbol());
+        if(stocks == null){
+            throw new IllegalStateException(stock.getSymbol() + "not bought");
+        }
+
+        if(stocks.size() < quauntity){
+            quauntity = stocks.size();
+        }
+
+        for(int index = quauntity - 1; index >= 0; index --){
+            stocks.remove(index);
+        }
+    }
+
+    public BigDecimal getCurrentValue(){
+        BigDecimal avgPrice = BigDecimal.ZERO;
+        for(String stockId : stockMap.keySet()){
+            for(Stock stock : stockMap.get(stockId)){
+                avgPrice = avgPrice.add(stock.getPrice());
+            }
+        }
+        return avgPrice;
     }
 }
